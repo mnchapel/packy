99e2ce624a795103fbf28a57663cbb924d4dc4af
Marie-Neige Chapel <marieneigecha@gmail.com>
2023-12-16 17:08:19 +0100

    Manage the partially checked for the files system model

--- src/model/files_model.py
+++ src/model/files_model.py
@@ -3,7 +3,8 @@ author: Marie-Neige Chapel
 """
 
 # PyQt
-from PyQt6.QtCore import Qt, QModelIndex
+from PyQt6 import QtCore
+from PyQt6.QtCore import Qt, QModelIndex, QDir
 from PyQt6.QtGui import QFileSystemModel
 
 ###############################################################################
@@ -12,6 +13,9 @@ class FilesModel(QFileSystemModel):
 	# -------------------------------------------------------------------------
 	def __init__(self, json_dict: dict = None, parent=None):
 		super(FilesModel, self).__init__()
+
+		filter = self.filter()
+		self.setFilter(filter | QDir.Filter.Hidden)
 
 		if json_dict is None:
 			self.defaultInitialization()
@@ -54,28 +58,82 @@ class FilesModel(QFileSystemModel):
 			return QFileSystemModel.data(self, index, role)
 		else:
 			if index.column() == 0:
-				return self.checkState(index)
+				return self.__checkState(index)
 	
 	# -------------------------------------------------------------------------
 	# @override
-	def setData(self, index: QModelIndex, value, role):
+	def setData(self, index: QModelIndex, value, role: Qt.ItemDataRole):
 		match role:
 			case Qt.ItemDataRole.CheckStateRole:
 				if index.column() == 0:
 					self.__checks[self.filePath(index)] = value
-
-					for i in range(self.rowCount(index)):
-						child_index = self.index(i, 0, index)
-						self.setData(child_index, value, role)
-					
 					self.dataChanged.emit(index, index)
+
+					self.__updateChildFiles(index, value, role)
+					self.__updateParentFiles(index, value)
+
 					return True
 				else:
-					print("[FilesModel] wrong index.column()")
+					QtCore.qDebug(f"Wrong index.column() = {index.column()}")
 					return False
 			case _:
-				print("[FilesModel] default case")
 				return QFileSystemModel.setData(self, index, value, role)
+	
+	# -------------------------------------------------------------------------
+	def __updateChildFiles(self, index: QModelIndex, value, role: Qt.ItemDataRole):
+		for i in range(self.rowCount(index)):
+			child_index = self.index(i, 0, index)
+			self.setData(child_index, value, role)
+
+	# -------------------------------------------------------------------------
+	def __updateParentFiles(self, index: QModelIndex, value):
+		match value:
+			case Qt.CheckState.Checked.value:
+				self.__propagateCheckToParents(index)
+			case Qt.CheckState.Unchecked.value:
+				self.__propagateUncheckToParents(index)
+			case _:
+				QtCore.qDebug(f"The Qt.CheckState value {value} should not appear in this function.")
+			
+	# -------------------------------------------------------------------------
+	def __propagateCheckToParents(self, index: QModelIndex):
+		index_parent = index.parent()
+		if index_parent != QModelIndex():
+			
+			all_siblings_checked = True
+			for i in range(self.rowCount(index_parent)):
+				index_sibling = self.index(i, 0, index_parent)
+				if self.__checkState(index_sibling) != Qt.CheckState.Checked.value:
+					all_siblings_checked = False
+			
+			if all_siblings_checked:
+				self.__updateCheckState(index_parent, Qt.CheckState.Checked)
+				self.__propagateCheckToParents(index_parent)
+			elif self.__checkState(index_parent) == Qt.CheckState.Unchecked.value:
+				self.__updateCheckState(index_parent, Qt.CheckState.PartiallyChecked)
+				self.__propagateCheckToParents(index_parent)
+	
+	# -------------------------------------------------------------------------
+	def __propagateUncheckToParents(self, index: QModelIndex) -> None:
+		index_parent = index.parent()
+
+		if self.__checkState(index_parent) == Qt.CheckState.Unchecked.value:
+			return
+		
+		at_least_one_selected_child: bool = False
+
+		for i in range(self.rowCount(index_parent)):
+			index_child = self.index(i, 0, index_parent)
+			if self.__checkState(index_child) != Qt.CheckState.Unchecked.value:
+				at_least_one_selected_child = True
+				break
+
+		new_check_state = Qt.CheckState.Unchecked
+		if at_least_one_selected_child:
+			new_check_state = Qt.CheckState.PartiallyChecked
+
+		self.__updateCheckState(index_parent, new_check_state)
+		self.__propagateUncheckToParents(index_parent)
 			
 	# -------------------------------------------------------------------------
 	# @override
@@ -83,11 +141,16 @@ class FilesModel(QFileSystemModel):
 		return QFileSystemModel.flags(self, index) | Qt.ItemFlag.ItemIsUserCheckable
 	
 	# -------------------------------------------------------------------------
-	def checkState(self, index):
+	def __checkState(self, index) -> int:
 		if self.filePath(index) in self.__checks:
 			return self.__checks[self.filePath(index)]
 		else:
-			return 0
+			return Qt.CheckState.Unchecked.value
+		
+	# -------------------------------------------------------------------------
+	def __updateCheckState(self, index: QModelIndex, check_state: Qt.CheckState) -> None:
+		self.__checks[self.filePath(index)] = check_state.value
+		self.dataChanged.emit(index, index)
 		
 	# -------------------------------------------------------------------------
 	def serialize(self):
