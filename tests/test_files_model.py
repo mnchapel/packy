c9e17654c220c44445db15416bc1d38f4eba6664
Marie-Neige Chapel <marieneigecha@gmail.com>
2023-12-28 17:51:30 +0100

    Some clean code

--- tests/test_files_model.py
+++ tests/test_files_model.py
@@ -21,20 +21,36 @@ from model.files_model import FilesModel
 # │  ├─ file_3.txt
 # │  └─ file_4.txt
 # └─ dir_3
-#    ├─ file_5.txt
-#    └─ file_6.txt
+#    ├─ dir_4.txt
+#    │  └─ file_5.txt
+#    ├─ file_6.txt
+#    └─ file_7.txt
 # ------------------------------
 
-DIR_PATHS = ["dir_1/",
-			 "dir_2/",
-			 "dir_3/"]
+DIR_1 = "dir_1/"
+FILE_1 = "dir_1/file_1.txt"
+FILE_2 = "dir_1/file_2.txt"
+DIR_2 = "dir_2/"
+FILE_3 = "dir_2/file_3.txt"
+FILE_4 = "dir_2/file_4.txt"
+DIR_3 = "dir_3/"
+DIR_4 = "dir_3/dir_4/"
+FILE_5 = "dir_3/dir_4/file_5.txt"
+FILE_6 = "dir_3/file_6.txt"
+FILE_7 = "dir_3/file_7.txt"
 
-FILE_PATHS = ["dir_1/file_1.txt",
-			"dir_1/file_2.txt",
-			"dir_2/file_3.txt",
-			"dir_2/file_4.txt",
-			"dir_3/file_5.txt",
-			"dir_3/file_6.txt"]
+DIR_PATHS = [DIR_1,
+			 DIR_2,
+			 DIR_3,
+			 DIR_4]
+
+FILE_PATHS = [FILE_1,
+			  FILE_2,
+			  FILE_3,
+			  FILE_4,
+			  FILE_5,
+			  FILE_6,
+			  FILE_7]
 
 CHECKED = Qt.CheckState.Checked.value
 PARTIALLY_CHECKED = Qt.CheckState.PartiallyChecked.value
@@ -105,13 +121,12 @@ def createJsonDict1(tmp_path) -> str:
 	json_dict = {}
 	json_dict["root_path"] = str(tmp_path)
 	check_dict = {}
-	check_dict[joinPath(tmp_path, DIR_PATHS[0])] = CHECKED # dir_1/
-	check_dict[joinPath(tmp_path, FILE_PATHS[0])] = CHECKED # dir_1/file_1.txt
-	check_dict[joinPath(tmp_path, FILE_PATHS[1])] = CHECKED # dir_1/file_2.txt
-	check_dict[joinPath(tmp_path, DIR_PATHS[1])] = PARTIALLY_CHECKED # dir_2/
-	check_dict[joinPath(tmp_path, FILE_PATHS[2])] = CHECKED # dir_2/file_3.txt
+	check_dict[joinPath(tmp_path, DIR_1)] = CHECKED
+	check_dict[joinPath(tmp_path, FILE_1)] = CHECKED
+	check_dict[joinPath(tmp_path, FILE_2)] = CHECKED
+	check_dict[joinPath(tmp_path, DIR_2)] = PARTIALLY_CHECKED
+	check_dict[joinPath(tmp_path, FILE_3)] = CHECKED
 	json_dict["check"] = check_dict
-	print(check_dict)
 
 	return json_dict
 
@@ -133,12 +148,12 @@ def createJsonDict2(tmp_path) -> str:
 	json_dict = {}
 	json_dict["root_path"] = str(tmp_path)
 	check_dict = {}
-	check_dict[joinPath(tmp_path, DIR_PATHS[0])] = CHECKED # dir_1/
-	check_dict[joinPath(tmp_path, FILE_PATHS[0])] = CHECKED # dir_1/file_1.txt
-	check_dict[joinPath(tmp_path, DIR_PATHS[1])] = PARTIALLY_CHECKED # dir_2/
-	check_dict[joinPath(tmp_path, FILE_PATHS[2])] = CHECKED # dir_2/file_3.txt
-	check_dict[joinPath(tmp_path, "dir_4/")] = CHECKED # dir_4/
-	check_dict[joinPath(tmp_path, "dir_4/file_7.txt")] = CHECKED # dir_4/file_7.txt
+	check_dict[joinPath(tmp_path, DIR_1)] = CHECKED
+	check_dict[joinPath(tmp_path, FILE_1)] = CHECKED
+	check_dict[joinPath(tmp_path, DIR_2)] = PARTIALLY_CHECKED
+	check_dict[joinPath(tmp_path, FILE_3)] = CHECKED
+	check_dict[joinPath(tmp_path, "dir_5/")] = CHECKED
+	check_dict[joinPath(tmp_path, "dir_5/file_8.txt")] = CHECKED
 	json_dict["check"] = check_dict
 
 	return json_dict
@@ -156,13 +171,16 @@ def testCheckIntegrity1(tmp_path):
 	added_items = warnings.addedItems()
 	removed_items = warnings.removedItems()
 
-	assert not len(added_items)
-	assert not len(removed_items)
+	expected_added_items = []
+	expected_removed_items = []
+
+	assert added_items == expected_added_items
+	assert removed_items == expected_removed_items
 		
 # -------------------------------------------------------------------------
 # Test check integrity with warnings
 #
-# expected_added_items = ["tmp_path/dir_1/file_1.txt"]
+# expected_added_items = ["tmp_path/dir_1/file_2.txt"]
 # expected_removed_items = ["tmp_path/dir_4/", "tmp_path/dir_4/file_7.txt""]
 # -------------------------------------------------------------------------
 def testCheckIntegrity2(tmp_path):
@@ -175,9 +193,9 @@ def testCheckIntegrity2(tmp_path):
 	added_items = warnings.addedItems()
 	removed_items = warnings.removedItems()
 
-	expected_added_items = [joinPath(tmp_path, FILE_PATHS[1])]
-	expected_removed_items = [joinPath(tmp_path, "dir_4/"),
-						   joinPath(tmp_path, "dir_4/file_7.txt")]
+	expected_added_items = [joinPath(tmp_path, FILE_2)]
+	expected_removed_items = [joinPath(tmp_path, "dir_5/"),
+						   joinPath(tmp_path, "dir_5/file_8.txt")]
 
 	assert added_items == expected_added_items
 	assert removed_items == expected_removed_items
