c293b3dc4a80f909348de6bf97d99724e4a5f542
Marie-Neige Chapel <marieneigecha@gmail.com>
2024-01-31 16:26:35 +0100

    Add icon to the app window.

--- packy/main.py
+++ packy/main.py
@@ -3,14 +3,17 @@ author: Marie-Neige Chapel
 """
 
 # Python
+import os
 import sys
 
 # PyQt
 from PyQt6 import QtCore, QtWidgets
+from PyQt6.QtGui import QIcon
 
 # PackY
 from model.log import messageHandler
 from view.main_window import MainWindow
+from utils.assets_access import assets_path
 
 # -----------------------------------------------------------------------------
 def initLog() -> None:
@@ -19,6 +22,8 @@ def initLog() -> None:
 
 # -----------------------------------------------------------------------------
 app = QtWidgets.QApplication(sys.argv)
+icon_path = os.path.join(assets_path(), "logo.ico")
+app.setWindowIcon(QIcon(icon_path))
 main_window = MainWindow()
 initLog()
 sys.exit(app.exec())
