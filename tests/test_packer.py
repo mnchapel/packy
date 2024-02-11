f605c1a1d85f2d9e313e355b658c4650e6bcfa57
Marie-Neige Chapel <marieneigecha@gmail.com>
2024-02-11 22:02:09 +0100

    Remove an old unecessary test.

--- tests/test_packer.py
+++ tests/test_packer.py
@@ -11,9 +11,8 @@ import json
 import os
 import pathlib
 import pytest
-import re
 from unittest.mock import MagicMock, Mock
-from zipfile import is_zipfile, ZipFile, ZipInfo
+from zipfile import ZipFile, ZipInfo
 
 # PyQt
 from PyQt6.QtCore import QStandardPaths
@@ -131,80 +130,6 @@ def checkArchiveHierarchy(loadTestData, outputPath, test_name):
 	expected_hierarchy = [output_name + "/" + item for item in expected_hierarchy]
 
 	assert zip.namelist() == expected_hierarchy
-
-###############################################################################
-# TEST ZIP PACKER
-#
-# -----------------------------------------------------------------------------
-# test_pack_file_1
-# -----------------------------------------------------------------------------
-#
-# Description:
-#		Pack a file at the root.
-#
-# Expected:
-#		tmp_path/output_<snapshot_retention_options>.zip
-#
-# -----------------------------------------------------------------------------
-# test_pack_file_2
-# -----------------------------------------------------------------------------
-#
-# Description:
-#		Pack a file inside a folder.
-#
-# Expected:
-#		tmp_path/output_<snapshot_retention_options>.zip
-#
-# -----------------------------------------------------------------------------
-# test_pack_folder_1
-# -----------------------------------------------------------------------------
-#
-# Description:
-#		Pack a folder and the files inside.
-#
-# Expected:
-#		tmp_path/output_<snapshot_retention_options>.zip
-#
-###############################################################################
-class TestZipPacker():
-
-	# -------------------------------------------------------------------------
-	@pytest.fixture
-	def runZipPacker(self, loadTestData, test_name):
-		task_dict = loadTestData[test_name]["data"]
-		task = Task(task_dict["id"], task_dict)
-		zip_packer = ZipPacker(task)
-		zip_packer.run()
-
-	# -------------------------------------------------------------------------
-	@pytest.fixture
-	def outputPath(self, loadTestData, test_name):
-		data = loadTestData[test_name]["expected"]
-		dirname = data["dst_folder"]
-
-		suffix_pattern_date = "_[0-9]{4}_[0-9]{2}_[0-9]{2}"
-		suffix_pattern_version = "_[0-9]+"
-
-		file_pattern_date = data["dst_raw_basename"] + suffix_pattern_date + ".zip"
-		file_pattern_version = data["dst_raw_basename"] + suffix_pattern_version + ".zip"
-
-		snapshot_date = [f for f in filter(re.compile(file_pattern_date).match, os.listdir(dirname))]
-		snapshot_version = [f for f in filter(re.compile(file_pattern_version).match, os.listdir(dirname))]
-
-		snapshot = snapshot_date + snapshot_version
-
-		if len(snapshot) == 1:
-			yield joinPath(dirname, snapshot[0])
-		else:
-			yield ""
-
-	# -------------------------------------------------------------------------
-	@pytest.mark.parametrize("test_name", ["test_pack_file_1", "test_pack_file_2", "test_pack_folder_1"])
-	def testPackFile(self, createFileHierarchy, runZipPacker, outputPath, checkArchiveHierarchy, test_name):
-		output_path = outputPath
-
-		assert output_path
-		assert is_zipfile(output_path)
 		
 ###############################################################################
 # TEST TMP FOLDER PATH
